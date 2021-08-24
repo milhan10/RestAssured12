20b0b86c8f010d8977623e72ca01261a288a090d
hp <ilhantuna10@gmail.com>
2021-08-24 21:50:06 +0200

    ilk commit

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -16,4 +17,65 @@ public class ZippoTest {
         ;
     }
 
+    @Test
+    public void statusCodeTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body() //log. all btün responsu gösterir
+                .statusCode(200) // status kontrolü
+
+                ;
+    }
+
+    @Test
+    public void contentTypeTest()
+    {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .contentType(ContentType.JSON)
+              ;
+    }
+
+    @Test
+    public void logTest()
+    {
+        given()
+                .log().all()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+        ;
+    }
+
+    @Test
+    public void checkStateInResponseBody()
+    {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .body("country",equalTo("United States"))//body.country == United States
+                .statusCode(200)
+        ;
+    }
+    @Test
+    public void californiaTest()
+    {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .body("places[0].state",equalTo("California"))//
+                .statusCode(200)
+        ;
+    }
 }
