d92e25fb6e1320eb49c319c29e857ebde8509459
hp <ilhantuna10@gmail.com>
2021-08-24 20:20:40 +0200

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class ZippoTest {
+
+    @Test
+    public void  test()
+    {
+        given()
+                //hazirlik islemleri yapacagiz
+                .when()
+                //link ve aksion islemleri
+                .then()
+                //test ve extrakt islemleri
+        ;
+    }
+
+}
